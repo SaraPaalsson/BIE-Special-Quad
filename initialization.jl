b723c2ba79a0b4b7a42b39c541399984c84b960e
SaPa <sara.paalsson@gmail.com>
2016-02-01 13:52:54 +0100

    Initialization finished

--- initialization.jl
+++ initialization.jl
@@ -1,40 +1,93 @@
 using JLD
 
-function indata_setup()
-#Computes initialization data and settings 
 
-#######################
-	#Parametrization	
-#######################
-# Starfish domain
-	tau(t) = (1+0.3*cos(5*(t))).*exp(im*(t))
-	taup(t) = (-1.5*sin(5*(t))+im*(1+0.3*cos(5*(t)))).*exp(im*(t));
-	taupp(t) = exp(im*(t)).*(-1-7.8*cos(5*(t))-(3*im)*sin(5*(t)));
+function indata_setup(Npanels,geomShape,filename_in)
+#= Compute initialization data and settings 
+	Input:		-Npanels, number of panels on interface
+				-geomShape, desired shape of interface (so far only starfish allowed) 
+	Saves parameters, settings and initial disc. points to indata/filename.jld
+=#
 
-	Npanels = 10 #Number of GL panels on interface
+	#Parametrization		
+	if geomShape == "starfish" #Starfish domain
+		fparm(t) = starfish_parm(t)
+	end
 
- 	(z,W) = domain_setup(Npanels,tau) #Disc. points and weights
+	#Distribute G.-L points on interface 
+ 	(z,W,panels,parm) = domain_setup(Npanels,fparm) #Disc. points and weights
 
-	s = linspace(0,2*pi,Npanels*16+1)
-	s = s[1:end-1]
- 	z1 = tau(s)
-
-
-#######################
 	#Save data to file for later use
-#######################
-	filename = "indata/test.jld"
-	save(filename,"Npanels",Npanels,"z",z,"s",s)
+	tmp = "indata/"
+	filename = tmp * filename_in
+	save(filename,"Npanels",Npanels,"z",z,"parm",parm)
 end
 
-function domain_setup(Np,f)
-#Compute disc.points and their quadrature weights for parametrization f
+function starfish_parm(tvec)
+#=  Parametrize interface as a starfish 
+	Input:		-parametrization vec. tvec [0,2pi]
+	Output:		-tau, interface points
+				-taup,taupp, first and second derivative of disc. points
+=#
+	tau = zeros(size(tvec)); tau = complex(tau)
+	taup = zeros(size(tvec)); taup = complex(taup)
+	taupp = zeros(size(tvec)); taupp = complex(taupp)
+	for (k in 1:size(tvec,1))
+		t = tvec[k]
+		tau[k] = (1+0.3*cos(5*t)).*exp(im*t)
+		taup[k] = (-1.5*sin(5*(t))+im*(1+0.3*cos(5*(t)))).*exp(im*(t));
+		taupp[k] = exp(im*(t)).*(-1-7.8*cos(5*(t))-(3*im)*sin(5*(t)));
+		k += 1
+	end
+	return tau,taup,taupp
+end
 
+
+function domain_setup(Np,f)
+#= Compute disc.points and their quadrature weights for 
+parametrization f
+	Input:		-Np, number of G.-L panels
+				-f, parametrization function
+	Output:		-z, disc. points (16 point G.-L)
+				-W, quadrature weights (16 point G.-L)
+				-panels, panel division in [0,2pi]
+				-parm, parametrization vector [0,2pi]
+=#
 	panels = linspace(0,2*pi,Np+1) #Divide interface into panels
 
+	parm = []; z = []; W = []
 	for (i in 1:Np)
-		(nodes,weights)
+		(nodes,weights) = gaussleg16(panels[i],panels[i+1])
+		append!(parm,nodes)
+		(tmp,tmp1,tmp2) = f(nodes)
+		append!(z,tmp)
+		append!(W,vec(weights))
 	end
-	return z,W
+	return z,W,panels,parm
+end
+
+function gaussleg16(a,b)
+#=	Compute 16 point Gauss-Legendre quadrature nodes and weights on 
+interval [a,b] (as done in Trefethen) 
+	Input:		-a,b, start and end of interval resp.
+	Output:		-nodes, quadrature points on interv. (a,b) (16-p G.-L.)
+				-weights, quadrature weights on interv. (a,b) (16-p G.-L.)
+=#
+	nvec = 1:15
+	beta = 0.5*(1-(2.0*nvec).^(-2)).^(-1/2)
+	Tn = diagm(beta,-1) + diagm(beta,1)
+	(D,V) = eig(Tn)
+	booltmp = !(D.==0)
+	nodes = []
+	for (i in 1:length(D))
+		if booltmp[i]
+			push!(nodes,D[i])
+		end
+	end
+	weights = (2*V[1,:].^2)'
+	#= NB here we could use our saved W16 weights and remap them instead... 
+	but we won't save much time doing so =#
+	nodes = (a*(1-nodes)+b*(1+nodes))/2
+	weights =(b-a)/2*weights
+	return nodes,weights
 end
 
